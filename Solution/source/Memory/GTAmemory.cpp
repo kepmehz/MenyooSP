84cabe16e0e5e9b07024d2e370f329964ecb5876
kepmehz <kepmehzde@gmail.com>
2021-06-14 20:04:42 +0200

    removed pointless nop (arxan checked)

--- Solution/source/Memory/GTAmemory.cpp
+++ Solution/source/Memory/GTAmemory.cpp
@@ -805,10 +805,6 @@ void GTAmemory::Init()
 	address = MemryScan::PatternScanner::FindPattern("48 85 C0 0F 84 ? ? ? ? 8B 48 50");
 	if (address) memset(reinterpret_cast<void*>(address), 0x90, 24);
 
-	// Bypass is player model allowed to spawn checks
-	address = MemryScan::PatternScanner::FindPattern("48 8B C8 FF 52 30 84 C0 74 05 48");
-	if (address) memset(reinterpret_cast<void*>(address + 0x8), 0x90, 2);
-
 	_SpSnow = SpSnow();
 
 }
